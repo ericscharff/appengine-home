015230be8443a35b14f8ce3c7ab7bb6acde6fb6f
Eric Scharff <eric.scharff@gmail.com>
2024-09-12 20:15:28 -0600

    Import home and calc apps
    These originally came from my github home repository,
    https://github.com/ericscharff/home but were modified so that they would
    server properly with flask.

--- main.py
+++ main.py
@@ -4,6 +4,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/calc/")
+def calc_main():
+    return render_template("calc/index.html")
+
+@app.route("/calc/manifest.json")
+def calc_manifest():
+    return render_template("calc/manifest.json")
+
+@app.route("/homeapp/")
+def homeapp_main():
+    return render_template("homeapp/index.html")
+
+@app.route("/homeapp/manifest.json")
+def homeapp_manifest():
+    return render_template("homeapp/manifest.json")
 
 @app.route("/")
 def root():
