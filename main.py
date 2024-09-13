166d197119210a5d65797b1ca37dc2919ecb4c0e
Eric Scharff <eric.scharff@gmail.com>
2024-09-12 20:59:33 -0600

    Add endpoints for index.html
    This is necessary because of the index.html in the manifest of the apps

--- main.py
+++ main.py
@@ -8,12 +8,20 @@ app = Flask(__name__)
 def calc_main():
     return render_template("calc/index.html")
 
+@app.route("/calc/index.html")
+def calc_index():
+    return render_template("calc/index.html")
+
 @app.route("/calc/manifest.json")
 def calc_manifest():
     return render_template("calc/manifest.json")
 
 @app.route("/homeapp/")
 def homeapp_main():
+    return render_template("homeapp/index.html")
+
+@app.route("/homeapp/index.html")
+def homeapp_index():
     return render_template("homeapp/index.html")
 
 @app.route("/homeapp/manifest.json")
