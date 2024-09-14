4c0f0a0ad17f34c403125303547932376c083950
Eric Scharff <eric.scharff@gmail.com>
2024-09-14 17:26:32 -0600

    Reformat code
    This is the result of using autopep8 like so:
      find . -name '*.py' -exec autopep8 --in-place --aggressive --aggressive {} \;
    
    This seemed to mostly fix indendation between functions.

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ from flask import Flask, make_response, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/calc/")
 @app.route("/calc/index.html")
 def calc_main():
     return render_template("calc/index.html")
+
 
 @app.route("/calc/manifest.json")
 def calc_manifest():
@@ -15,16 +17,19 @@ def calc_manifest():
     resp.mimetype = 'application/json'
     return resp
 
+
 @app.route("/homeapp/")
 @app.route("/homeapp/index.html")
 def homeapp_main():
     return render_template("homeapp/index.html")
+
 
 @app.route("/homeapp/manifest.json")
 def homeapp_manifest():
     resp = make_response(render_template("homeapp/manifest.json"))
     resp.mimetype = 'application/json'
     return resp
+
 
 @app.route("/")
 def root():
--- main_test.py
+++ main_test.py
@@ -2,6 +2,7 @@ import pytest
 
 import main
 
+
 @pytest.fixture()
 def app():
     app = main.app
@@ -11,34 +12,42 @@ def app():
 
     yield app
 
+
 @pytest.fixture()
 def client(app):
     return app.test_client()
+
 
 def test_main(client):
     response = client.get("/")
     assert b'Starting Page' in response.data
 
+
 def test_calc_main(client):
     response = client.get("/calc/")
     assert b'Calculator' in response.data
 
+
 def test_calc_index(client):
     response = client.get("/calc/index.html")
     assert b'Calculator' in response.data
+
 
 def test_calc_manifest(client):
     response = client.get("/calc/manifest.json")
     assert response.mimetype == 'application/json'
     assert b'AECalc' in response.data
 
+
 def test_homeapp_main(client):
     response = client.get("/homeapp/")
     assert b'Home' in response.data
 
+
 def test_homeapp_index(client):
     response = client.get("/homeapp/index.html")
     assert b'Home' in response.data
+
 
 def test_homeapp_manifest(client):
     response = client.get("/homeapp/manifest.json")
