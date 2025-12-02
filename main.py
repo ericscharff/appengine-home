a23d4a07c3ead3372954278bdc9bed74b918dc66
Eric Scharff <eric.scharff@gmail.com>
2025-12-01 19:05:03 -0700

    1-Dec Update

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from flask import Flask, make_response, render_template, request
 
 app = Flask(__name__)
 
-BUILD_DATE = "Last modified Sun Nov 30 07:19:24 AM MST 2025"
+BUILD_DATE = "Last modified Mon Dec  1 07:04:59 PM MST 2025"
 
 
 @app.route("/calc")
