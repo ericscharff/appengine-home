6b06379e6fcf05f659583b6006ad9a40d52dc0ee
Eric Scharff <eric.scharff@gmail.com>
2026-06-29 18:12:59 -0600

    Get tests passing again
    When I removed the manifests (no more PWA) I did not remove the serving
    actions. They were never called, so it wasn't an issue.
    
    While I was at it, I removed some python deprecation warnings involving
    how I was using time objects.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime, timedelta, timezone
 import secrets
 
 from flask import Flask, make_response, render_template, request
@@ -15,19 +15,12 @@ def calc_main():
     return render_template("calc/index.html")
 
 
-@app.route("/calc/manifest.json")
-def calc_manifest():
-    resp = make_response(render_template("calc/manifest.json"))
-    resp.mimetype = "application/json"
-    return resp
-
-
 @app.route("/homeapp")
 @app.route("/homeapp/")
 @app.route("/homeapp/index.html")
 def homeapp_main():
     current_date = (
-        datetime.datetime.utcnow() + datetime.timedelta(hours=-6)
+        datetime.now(timezone.utc) + timedelta(hours=-6)
     ).isoformat()
     return render_template("homeapp/index.html", current_date=current_date)
 
@@ -42,13 +35,6 @@ def homeapp_runner():
     return render_template("homeapp/runner.html")
 
 
-@app.route("/homeapp/manifest.json")
-def homeapp_manifest():
-    resp = make_response(render_template("homeapp/manifest.json"))
-    resp.mimetype = "application/json"
-    return resp
-
-
 @app.route("/check_token")
 def check_update():
     message = f"Your token was {request.args.get('token', 'nothing')}."
@@ -58,7 +44,7 @@ def check_update():
 @app.route("/")
 def root():
     current_date = (
-        datetime.datetime.utcnow() + datetime.timedelta(hours=-6)
+        datetime.now(timezone.utc) + timedelta(hours=-6)
     ).isoformat()
     return render_template(
         "index.html", build_date=BUILD_DATE, current_date=current_date
--- main_test.py
+++ main_test.py
@@ -35,12 +35,6 @@ def test_calc_index(client):
     assert b"Calculator" in response.data
 
 
-def test_calc_manifest(client):
-    response = client.get("/calc/manifest.json")
-    assert response.mimetype == "application/json"
-    assert b"AECalc" in response.data
-
-
 def test_homeapp_main(client):
     response = client.get("/homeapp")
     assert b"Home" in response.data
@@ -49,9 +43,3 @@ def test_homeapp_main(client):
 def test_homeapp_index(client):
     response = client.get("/homeapp/index.html")
     assert b"Home" in response.data
-
-
-def test_homeapp_manifest(client):
-    response = client.get("/homeapp/manifest.json")
-    assert response.mimetype == "application/json"
-    assert b"AEHome" in response.data
